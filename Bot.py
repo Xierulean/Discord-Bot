3b4f68d9520fc1e6fd47e7e382ffa42c6331a66b
DMCTruong <diane.m.truong@gmail.com>
2017-07-18 16:22:22 -0500

    Added some bot functions

--- Bot.py
+++ Bot.py
@@ -5,6 +5,10 @@ import asyncio
 
 bot = commands.Bot(command_prefix="/")
 
+# Note to self: \ = new line for python
+print("Please wait while the Bot logs in ...")
+
+
 @bot.event
 async def on_ready():
 	print("Logged in as:")
@@ -12,8 +16,36 @@ async def on_ready():
 	print(bot.user.id)
 	print("========================")
 
+# Say "Testing? Testing..."
 @bot.command()
 async def test():
 	await bot.say("Testing? Testing...")
 
+# Say :joy: then echo what user said
+@bot.command(pass_context = True)
+async def echo2(ctx, *, echo: str):
+	await bot.delete_message(ctx.message)
+	await bot.say(":joy:" + echo)
+
+# Playfully kill a member
+@bot.command(pass_context = True)
+async def kill(ctx, *, member : discord.Member =  None):
+	if member is None:
+		await bot.say(ctx.message.author.mention + ": I cannot kill someone unless you tell me who you want to kill!")
+		return
+		
+	if member.id == "319302490518454272":
+		await bot.say(ctx.message.author.mention + ": You can't kill me if I kill you first!")
+	elif member.id == "164202667118297090":
+		await bot.say("You can't kill my master!")
+	elif member.id == "164202667118297090" and member.id == ctx.member.id:
+		await bot.say(ctx.message.author.mention + ": A-Are you sure mom?")
+	elif member.id == ctx.message.author:
+		await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
+	else: 
+		random.seed(time.time())
+		choice = killResponses[random.randint(0, len(killResponses) - 1)] % member.mention
+		await bot.say(ctx.message.author.mention + ": " + choice)
+# Stopping here temporarily to download the Discord API wrapper 
+	
 bot.run("Insert bot token here.")
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Discord-Bot
 A text bot created for Discord voice and chat app. Its purpose is to act as a personal helper. Right now, there's not a lot of functions but more is currently being added.
 
+Requirements:
+Python
+Discord.py
+
+To install Discord.py, open a command line after installing python and type: python -m pip install -U .[voice]
+
 
