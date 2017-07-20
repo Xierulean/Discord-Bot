08c60b69464efd3c38a6589f16ec3d047ab3204f
DMCTruong <diane.m.truong@gmail.com>
2017-07-19 23:39:15 -0500

    Added changelog and installed Discord.py

--- src/Bot.py
+++ src/Bot.py
@@ -2,8 +2,11 @@ import discord
 from discord.ext import commands
 import logging
 import asyncio
+import random
+import time
 
 bot = commands.Bot(command_prefix="/")
+killResponses = [" threw %s thrown off the boat!", " gave %s a headbutt!", " unleashed a Kraken on %s!", " gave %s a bearhug!"]
 
 # Note to self: \ = new line for python
 print("Please wait while the Bot logs in ...")
@@ -35,17 +38,27 @@ async def kill(ctx, *, member : discord.Member =  None):
 		return
 		
 	if member.id == "319302490518454272":
-		await bot.say(ctx.message.author.mention + ": You can't kill me if I kill you first!")
+		await bot.say(ctx.message.author.mention + ": You can't kill me!")
+	elif member.id == "164202667118297090" and member.id == ctx.message.author.id:
+		await bot.say(ctx.message.author.mention + ": A-Are you sure you want me to kill you mom?")
 	elif member.id == "164202667118297090":
 		await bot.say("You can't kill my master!")
-	elif member.id == "164202667118297090" and member.id == ctx.member.id:
-		await bot.say(ctx.message.author.mention + ": A-Are you sure mom?")
 	elif member.id == ctx.message.author:
 		await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
 	else: 
 		random.seed(time.time())
-		choice = killResponses[random.randint(0, len(killResponses) - 1)] % member.mention
-		await bot.say(ctx.message.author.mention + ": " + choice)
-# Stopping here temporarily to download the Discord API wrapper 
+		choice = killResponses[random.randrange(len(killResponses))] % member.mention
+		await bot.say(ctx.message.author.mention + " " + choice)
+                
+@bot.command(pass_context = True)
+async def hug(ctx, *, member : discord.Member = None):
+        if member is None:
+                await bot.say(ctx.message.author.mention + " gave a hug.")
+        else:
+            if member.id == ctx.message.author.id:
+                await bot.say("A bear gave " + member.mention + " a bear hug.")
+            else:
+                await bot.say(member.mention + " has been hugged by " + ctx.message.author.mention + ".")
 	
 bot.run("Insert bot token here.")
+
