c19d40b279e59375db2e810bc2d7e317fdb6efca
DMCTruong <diane.m.truong@gmail.com>
2017-07-25 19:57:15 -0500

    See docs/changelog.txt for info

--- src/Bot.py
+++ src/Bot.py
@@ -1,104 +1,151 @@
-from urllib.parse import urlencode
+##########################################################################################
+# Program Name:		Discord Bot
+# Author:			DMCTruong
+# Last Updated:		July 25, 2017
+# Description:		A bot that was created for Discord chat with various functions
+##########################################################################################
+
+
+# To do List:
+#	- Add a tutorial on how to install and get the keys for the configurations
+#	- Add better documentation of the code
+#	- Add bot help command
+#	- Create a database for Vindictus's scroll price estimates (with permmissions)
+#	- Get Firebase DB to work with the bot
+#	- Reorganize the code or separate the code into different files
+#	- Update the README.md
+
+
 import discord
 from discord.ext import commands
+from urllib.parse import urlencode
 import logging
 import asyncio
+import configurations
 import random
 import time
 
-bot = commands.Bot(command_prefix="/")
-console = discord.Object("319302490518454272")
-killResponses = [" threw %s thrown off the boat!", " gave %s a headbutt!", " unleashed a Kraken on %s!", " gave %s a bearhug!"]
 
-# Note to self: \ = new line for python
-print("Please wait while the Bot logs in ...")
+bot = commands.Bot(configurations.PREFIX)
 
+eightBallResponses = [
+    "Sure, go for it!",
+    "Sure but I'm not entirely certain",
+    "Perhaps?",
+    "No, that's a terrible idea!",
+    ]
+
+funnyPrefix = [
+    "Pink Unicorn ",
+    "Papa Smurf ",
+    "King ",
+    "Queen ",
+    "Prince ",
+    "Princess, "
+    ]
+killResponses = [
+    " threw %s thrown off the boat!",
+    " gave %s a headbutt!",
+    " unleashed a Kraken on %s!",
+    " gave %s a bearhug!"
+    ]
+
+
+# Upon login, say ...
 @bot.event
 async def on_ready():
-	print("Logged in as:")
-	print(bot.user.name)
-	print(bot.user.id)
-	print("========================")
-
-# Say "Testing? Testing..."
-@bot.command()
-async def test():
-	await bot.say("Testing? Testing...")
-
-# Say :joy: then echo what user said
-@bot.command(pass_context = True)
-async def echo2(ctx, *, echo: str):
-	await bot.delete_message(ctx.message)
-	await bot.say(":joy:" + echo)
-
-# Playfully kill a member
-@bot.command(pass_context = True)
-async def kill(ctx, *, member : discord.Member =  None):
-	if member is None:
-		await bot.say(ctx.message.author.mention + ": I cannot kill someone unless you tell me who you want to kill!")
-		return
+    print("Logged in as:")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("========================")
+    
+# Command: Say :joy: and echo what user said
+@bot.command(pass_context=True)
+async def echo(ctx, *, echo: str):
+    await bot.delete_message(ctx.message)
+    await bot.say(":joy:" + echo)
+    
+# Command: Playfully interact with a member
+@bot.command(pass_context=True)
+async def kill(ctx, *, member: discord.Member=None):
+    if member is None:
+        await bot.say(ctx.message.author.mention + ": I cannot kill someone unless you tell me who you want to kill!")
+        return
 		
-	if member.id == "319302490518454272":
-		await bot.say(ctx.message.author.mention + ": You can't kill me!")
-	elif member.id == "164202667118297090" and member.id == ctx.message.author.id:
-		await bot.say(ctx.message.author.mention + ": A-Are you sure you want me to kill you mom?")
-	elif member.id == "164202667118297090":
-		await bot.say("You can't kill my master!")
-	elif member.id == ctx.message.author:
-		await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
-	else: 
-		random.seed(time.time())
-		choiceResponse = killResponses[random.randrange(len(killResponses))] % member.mention
-		await bot.say(ctx.message.author.mention + " " + choiceResponse)
+    if member.id == configurations.BOT_ID:
+        await bot.say(ctx.message.author.mention + ": You can't kill me!")
+    elif member.id == configurations.BOT_OWNER_ID and member.id == ctx.message.author.id:
+        await bot.say(ctx.message.author.mention + ": A-Are you sure you want me to kill you mom?")
+    elif member.id == configurations.BOT_OWNER_ID:
+        await bot.say("You can't kill my master!")
+    elif member.id == ctx.message.author:
+        await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
+    else: 
+        random.seed(time.time())
+        choiceResponse = killResponses[random.randrange(len(killResponses))] % member.mention
+        await bot.say(ctx.message.author.mention + " " + choiceResponse)
  
-# Hug someone or yourself
-@bot.command(pass_context = True)
-async def hug(ctx, *, member : discord.Member = None):
-        if member is None:
-                await bot.say(ctx.message.author.mention + " gave a hug.")
+# Command: Hug someone or yourself
+@bot.command(pass_context=True)
+async def hug(ctx, *, member: discord.Member=None):
+    if member is None:
+        await bot.say(ctx.message.author.mention + " gave a hug.")
+    else:
+        if member.id == ctx.message.author.id:
+            await bot.say("A bear gave " + member.mention + " a bear hug.")
         else:
-            if member.id == ctx.message.author.id:
-                await bot.say("A bear gave " + member.mention + " a bear hug.")
-            else:
-                await bot.say(member.mention + " has been hugged by " + ctx.message.author.mention + ".")
-                
-# Make the bot pick a choice in the form of: choice1,choice2
-@bot.command(pass_context = True)
-async def choice(ctx, *, choices: str):
+            await bot.say(member.mention + " has been hugged by " + ctx.message.author.mention + ".")    
+	  
+# Command: Make the bot pick a choice in the form of: choice1,choice2
+@bot.command(pass_context=True)
+async def pick(ctx, *, choices: str):
     choicesArr = choices.split(" or ")
     chosen = choicesArr[random.randrange(len(choicesArr))]
     await bot.say(ctx.message.author.mention + ": I choose " + chosen)
   
-# Add "instanceof" to everyone's nickname in the server
-@bot.command(pass_context = True)
-async def instanceof(ctx):
+# Command: Add a funny prefix to everyone's nickname in the server as a joke
+@bot.command(pass_context=True)
+async def trollEveryone(ctx):
     for member in ctx.message.server.members:
         try:
-            await bot.change_nickname(member, "instanceof " + member.name)
+            random.seed(time.time())
+            addPrefix = funnyPrefix[random.randrange(len(funnyPrefix))]		
+            await bot.change_nickname(member, addPrefix + member.name)
         except discord.errors.Forbidden:
             pass
-    await bot.say(ctx.message.author.mention + ": Everyone's nickname is changed!")
-
-# Return everyone's nickname in the server back to normal 
-@bot.command(pass_context = True)
-async def noinstance(ctx):
+    await bot.say(ctx.message.author.mention + ": Everyone's nickname is changed!! Buahahaha")
+	
+# Command: Return everyone's nickname in the server back to normal after using /trollEveryone command
+@bot.command(pass_context=True)
+async def noMoreTroll(ctx):
     for member in ctx.message.server.members:
         try:
             await bot.change_nickname(member, member.name)
         except discord.errors.Forbidden:
             pass
-    await bot.say(ctx.message.author.mention + ": Everyone's nickname is now back to normal!")
-    
-# Command that lets the user to search through Python's documentation
-@bot.command()
+    await bot.say(ctx.message.author.mention + ": Everyone's nickname is now changed back to normal!")
+ 
+ 
+# Command: Lets the user to search through Python's documentation
+@bot.command(aliases=["py_help", "pyh", "python_help"])
 async def pyhelp(*args):
     print(args)
     url = ("https://docs.python.org/3/search.html?{}"
-            "&check_keywords=yes&area=default".format(
-                urlencode({'q': ' '.join(args)})
-            ))
+           "&check_keywords=yes&area=default".format(
+           urlencode({'q': ' '.join(args)})
+           ))
     return await bot.say(url)
-    
-bot.run("Insert bot token here.")
 
+	
+# Command: "8ball"
+@bot.command(pass_context=True)
+async def ask(ctx):
+    random.seed(time.time())
+    askResponse = eightBallResponses[random.randrange(len(eightBallResponses))]
+    await bot.say(ctx.message.author.mention + " " + askResponse)
 
+# Command: Bot Help, returns all available commands
+async def help(ctx):
+    await bot.say()
+
+bot.run(configurations.BOT_TOKEN)
---
+++ src/configurations.py
@@ -0,0 +1,4 @@
+PREFIX = "/"
+BOT_OWNER_ID = "Bot Owner's ID here"
+BOT_ID = "Bot's ID here"
+BOT_TOKEN = "Bot's Token here"
