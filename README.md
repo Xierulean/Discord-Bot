8a1042a712e973f85a98ef18e17f3f700e003eed
DMCTruong <diane.m.truong@gmail.com>
2017-07-27 17:08:47 -0500

    Taught the bot how to sing!

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Discord-Bot
-A text bot created for Discord voice and chat app. Its purpose is to act as a personal helper. Right now, there's not a lot of functions but more is currently being added.
+A general purpose bot created for Discord.
 
-Requirements:
-Python
-Discord.py
+# Requirements
 
-To install Discord.py, open a command line after installing python and type: python -m pip install -U .[voice]
+1. Python
+2. Discord.py
+3. pyrebase
 
+# Installation
+
+1. Download and install the lastest version of Python. Be sure to add Python's path to the Enviromental Variables!
+1. Next, to install Discord.py, open a command line and type: python -m pip install -U .[voice]
+2. Then, install pyrebase by typing the following in a command line: pip install pyrebase
+3. After that, open /docs/configurations.py (preferably with Notepad++ or a similar program)and the tutorials in the file will help you retrieve the information that the bot requires to run.
+4. Once all of the information in the configurations file is filled out, the bot is ready to run, double click Bot.py! For more information on what commands the bot can use, type /help in the Discord chat.
 
--- src/configurations.py
+++ src/configurations.py
@@ -1,26 +1,38 @@
-# For now, the default command prefix is "/"
-# Example: /echo Hello There
-# You are free to change the command prefix to anything you wish.
+##########################################################################################
+# Program Name:				Discord Bot
+# Author:					DMCTruong
+# Last Updated:				July 27, 2017
+# Program Description:		A general purpose bot that was created for Discord chat	
+##########################################################################################
 
+
+# <-------------------------- Discord Bot Configurations --------------------------------->
+
+# The following are information that the bot requires before the bot can run.
+# Once all of the following information is filled out, the bot will be ready to use. Just 
+# double click on Bot.py and you will be able to use all of the bot commands in discord! 
+
+# For more information about what commands the bot can use, type /commands or /help in 
+# the Discord chat.
+
+# <--------------------------------------------------------------------------------------->
 
 PREFIX = "/"
 
-###################################################################################################
-# Please follow this tutorial to get various Discord IDs
-# https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
+# Currently, the default command prefix is "/"
+# For example: /echo Hello World
+# You are welcome to change the command prefix to anything you wish.
 
 BOT_OWNER_ID = "Bot Owner's ID here"
 BOT_ID = "Bot's ID here"
 
-###################################################################################################
-# Follow this tutorial to get a Discord Bot token
-# https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
+# Please follow this tutorial to get various Discord IDs.
+# https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
 
 BOT_TOKEN = "Bot's Token here"
 
-###################################################################################################
-# To get Firebase info, follow this tutorial until you get all of the information to fill out the missing data below
-# https://firebase.google.com/docs/web/setup
+# This tutorial will show how to get a Discord Bot token.
+# https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
 
 FIREBASE_INFO = {
     "apiKey": "Firebase API Key here",
@@ -31,6 +43,17 @@ FIREBASE_INFO = {
     "messagingSenderId": "Firebase Messaging Sender ID here"
 }
 
-###################################################################################################
+# To get Firebase info, please follow the tutorial below. You do not need to follow the entire tutorial, 
+# it is only until you get all of the information needed to fill out the required data above.
+# https://firebase.google.com/docs/web/setup
+
+
+# <--------------------------------------------------------------------------------------->
 
 # Thank you for using DMCTruong's Discord Bot!
+
+# For support, please submit a "New Issue" to the link below and I will do my best to help
+# https://github.com/DMCTruong/Discord-Bot/issues
+
+
+# <--------------------------------------------------------------------------------------->
